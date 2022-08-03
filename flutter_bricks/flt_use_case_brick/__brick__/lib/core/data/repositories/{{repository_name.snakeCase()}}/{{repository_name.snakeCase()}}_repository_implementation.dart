379940f0ce73085c429e8de70fb9f061c09ba8bb
Denis Viana <denisvcosta@gmail.com>
2022-08-03 15:27:23 -0300

    Fix syntax errors

--- flutter_bricks/flt_use_case_brick/__brick__/lib/core/data/repositories/{{repository_name.snakeCase()}}/{{repository_name.snakeCase()}}_repository_implementation.dart
+++
@@ -1,8 +0,0 @@
-import '../../../domain/repositories/{{repository_name.snakeCase()}}/{{repository_name.snakeCase()}}_repository.dart';
-
-class {{repository_name.pascalCase()}}RepositoryImplementation implements {{repository_name.pascalCase()}}Repository{
-
-  @override
-  Future<dynamic> {{name.camelCase()}}({required String property});
-
-}
--- flutter_bricks/flt_use_case_brick/__brick__/lib/core/domain/repositories/{{repository_name.snakeCase()}}/{{repository_name.snakeCase()}}_repository.dart
+++
@@ -1,6 +0,0 @@
-
-abstract class {{repository_name.pascalCase()}}Repository {
-
-  Future<dynamic> {{name.camelCase()}}({required String property});
-
-}
